eef4d588789d6bd38e4c211cdd1ef5e91798c889
nileshbohra <nmbohra2001@gmail.com>
2021-03-06 12:12:42 +0530

    integrated netlify

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import FormGroup from "./components/FormGroup";
 import Snackbar from "@material-ui/core/Snackbar";
 import MuiAlert from "@material-ui/lab/Alert";
+import Slide from "@material-ui/core/Slide";
 
 export default function Contact() {
   function Alert(props) {
@@ -22,63 +23,64 @@ export default function Contact() {
   };
 
   return (
-    <section id="Contact">
-      <h1 className="primaryTxt heading">Contact Me</h1>
-      <div className="contactForm">
-        <form name="contact" method="POST" netlify onSubmit={(e)=>{
-          e.preventDefault();
-          handleClick();
-        }}>
-          <div className="row">
-            <div className="col">
-              <FormGroup
-                labelFor="name"
-                labelText="Name"
-                inputType="text"
-                inputName="name"
-                inputPlaceholder="Enter Your Name"
-                required={true}
-              />
+    <Slide in={true} direction="up" timeout={1000}>
+      <section id="Contact">
+        <h1 className="primaryTxt heading">Contact Me</h1>
+        <div className="contactForm">
+          <form name="contact" method="post">
+            <input type="hidden" name="form-name" value="contact" />
+            <div className="row">
+              <div className="col">
+                <FormGroup
+                  labelFor="name"
+                  labelText="Name"
+                  inputType="text"
+                  inputName="name"
+                  inputPlaceholder="Enter Your Name"
+                  required={true}
+                />
+              </div>
+              <div className="col">
+                <FormGroup
+                  labelFor="email"
+                  labelText="Email"
+                  inputType="email"
+                  inputName="email"
+                  inputPlaceholder="Enter Your Email Address"
+                  required={true}
+                />
+              </div>
             </div>
-            <div className="col">
-              <FormGroup
-                labelFor="email"
-                labelText="Email"
-                inputType="email"
-                inputName="email"
-                inputPlaceholder="Enter Your Email Address"
-                required={true}
-              />
-            </div>
-          </div>
 
-          <FormGroup
-            labelFor="subject"
-            labelText="Subject"
-            inputType="text"
-            inputName="subject"
-            inputPlaceholder="Type Your Subject"
-          />
-          <div className="formGroup">
-            <label htmlFor="message">Your Message</label>
-            <textarea
-              name="message"
-              id="message"
-              rows="8"
-              placeholder="Type Your Message"
-              required
-            ></textarea>
-          </div>
-          <button className="btn formGroup" type="submit">
-            Submit
-          </button>
-          <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-            <Alert onClose={handleClose} severity="success">
-              This is a success message!
-            </Alert>
-          </Snackbar>
-        </form>
-      </div>
-    </section>
+            <FormGroup
+              labelFor="subject"
+              labelText="Subject"
+              inputType="text"
+              inputName="subject"
+              autoComplete="off"
+              inputPlaceholder="Type Your Subject"
+            />
+            <div className="formGroup">
+              <label htmlFor="message">Your Message</label>
+              <textarea
+                name="message"
+                id="message"
+                rows="8"
+                placeholder="Type Your Message"
+                required
+              ></textarea>
+            </div>
+            <button className="btn formGroup btnSubmit" type="submit">
+              Submit
+            </button>
+            <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+              <Alert onClose={handleClose} severity="success">
+                This is a success message!
+              </Alert>
+            </Snackbar>
+          </form>
+        </div>
+      </section>
+    </Slide>
   );
 }
