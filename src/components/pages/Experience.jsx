92cebbf52836025cebcb2b330ae990f6e831e558
Nilesh <nmbohra2000@gmail.com>
2022-10-10 23:08:45 +0530

    ui changes

--- src/components/pages/Experience.jsx
+++ src/components/pages/Experience.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import Slide from "@material-ui/core/Slide";
 
 export default function Experience() {
-  return <div>Experience</div>;
+  return (
+    <Slide in={true} direction="up" timeout={1000}>
+      <section id="Experience">
+        <h1 className="primaryTxt">UNDER CONSTRUCTION</h1>
+      </section>
+    </Slide>
+  );
 }
